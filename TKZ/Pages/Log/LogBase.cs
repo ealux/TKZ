c99ad4570e8ec7f43db54ecdec8d3870d548b1ec
Егор Люханов <ealuhanov@yandex.ru>
2020-04-11 02:58:04 +0500

    Stage 1 - Design - Log function async release

--- TKZ/Pages/Log/LogBase.cs
+++ TKZ/Pages/Log/LogBase.cs
@@ -12,39 +12,22 @@ namespace TKZ.Pages.Log
 {
     public class LogBase
     {
-        public static ObservableCollection<Message> Messages { get; set; }      
+        public ObservableCollection<Message> Messages { get; set; }      
 
         public LogBase()
         {
             Messages = new ObservableCollection<Message>()
             {
                 new Message(Message.MessageType.Success, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Warning, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Danger, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Success, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Warning, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Danger, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Success, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Warning, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Danger, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Success, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Warning, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Danger, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Success, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Warning, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Danger, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Success, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Warning, "Внимание!", "У нас кончилась водка!"),
-                new Message(Message.MessageType.Danger, "Внимание!", "У нас кончилась водка!")
             };
         }
 
-        public static async Task AddMessage(Message mes)
+        public async Task AddMessage(Message mes)
         {
             await Task.Run(()=>Messages.Add(mes));
         }
 
-        public static async Task RemoveMessage()
+        public async Task RemoveMessage()
         {
             await Task.Run(()=>Messages.Clear());//Messages.Remove(Messages.Where((m) => m.elem.Id == el.Id).First());
         }
@@ -53,7 +36,7 @@ namespace TKZ.Pages.Log
         /// Change log image source depending on the log messages count
         /// </summary>
         /// <returns><b>string</b>: Log image source</returns>
-        public static string ImageChecker()
+        public string ImageChecker()
         {
             string src = Task.Run(() => Messages.Count == 0 ? "img/bell.svg" : "img/bell_content.svg").GetAwaiter().GetResult();
             return src;
