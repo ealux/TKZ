fc490da448877ea782856498e39268578ab61f34
Егор Люханов <ealuhanov@yandex.ru>
2020-04-15 11:44:48 +0500

    Stage 1 - Design - Parser for Calcilation nodes input

---
+++ Client/Pages/Calc/CalculationRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TKZ.Client.Pages.Calc
+{
+    /// <summary>
+    /// Wrap on calculation node input.
+    /// </summary>
+    public class CalculationRequest
+    {
+
+        //TODO: После реализации модели коллекции узлов - дописать класс
+
+        /// <summary>
+        /// Collection of oninput generated integers (nodes)
+        /// </summary>
+        public string Request { get; set; }
+
+        /// <summary>
+        /// Convert non-empty request into List of Integers
+        /// </summary>
+        /// <returns>List of Integers related to the Request property</returns>
+        private List<int> GetList()
+        {
+            if (!String.IsNullOrEmpty(this.Request))
+            {
+                List<int> outputIntegers = new List<int>();
+
+                foreach (string item in this.Request.Split(","))
+                {
+                    outputIntegers.Add(Convert.ToInt32(item));
+                }
+
+                return outputIntegers;
+            }
+            else return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<int> GetActiveNodes()   //TODO
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
