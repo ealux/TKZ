b6520be791864c0ab5b8d270022440c021a69cfe
Alexander <Windsl@users.noreply.github.com>
2020-04-22 02:15:04 +0500

    Create alg Find Branch Belt
    Algorithm for searching branches in the network by a given belt and start node has been added

--- Shared/Model/Grid.cs
+++ Shared/Model/Grid.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using TKZ.Shared.Model;
 using System.Globalization;
@@ -45,6 +46,56 @@ namespace TKZ.Shared
 
             this.Mutuals.Add(m1.Id,m1);
             this.Mutuals.Add(m2.Id,m2);
-        }       
+        }      
+
+        public List<int> FindBranchBelt(int idBus, int numBelt=1)
+        {
+            if (numBelt <1) numBelt = 1;
+
+            Dictionary<int,bool> dicBranches = new Dictionary<int, bool>();
+            Dictionary<int,bool> dicBuses = new Dictionary<int, bool>();
+            foreach (Branch b in Branches.Values.ToList()) dicBranches.Add(b.Id,false);
+            foreach (Bus b in Buses.Values.ToList()) dicBuses.Add(b.Id,false);
+
+            RecursiveFindBeltBranch(dicBranches,dicBuses,idBus,numBelt);
+            
+            List<int> res = new List<int>();                        
+            foreach (int i in dicBranches.Keys) if (dicBranches[i] == true) res.Add(i);            
+            return res;
+        }   
+        private void RecursiveFindBeltBranch(Dictionary<int,bool> dicBranches,Dictionary<int,bool> dicBuses,int BusId,int Belt)
+        {
+            dicBuses[BusId] = true;
+            if (Belt < 1) return;
+
+            int[] l = FindAllBranchOnBusID(BusId);
+            for (int ind = 0; ind < l.Count(); ind++)
+            {
+                int cur = l[ind];
+                dicBranches[cur] = true;
+                if (Branches[cur].StIdBus  == BusId)
+                {
+                    if (dicBuses[Branches[cur].FinIdBus]==false) RecursiveFindBeltBranch (dicBranches, dicBuses,Branches[cur].FinIdBus,Belt - 1);
+                    continue;
+                }
+                if (Branches[cur].FinIdBus == BusId) 
+                {
+                    if (dicBuses[Branches[cur].StIdBus]==false) RecursiveFindBeltBranch (dicBranches, dicBuses,Branches[cur].StIdBus, Belt - 1);
+                    continue;
+                }
+            }
+        }
+        private int[] FindAllBranchOnBusID(int BusId)
+        {            
+            List<int> br = new List<int>();
+            foreach (Branch b in Branches.Values )
+            {
+                if (b.StIdBus ==BusId ) br.Add(b.Id);
+                if (b.FinIdBus==BusId) br.Add(b.Id);
+            }
+            int[] res = br.Distinct().ToArray();
+            Array.Sort(res);
+            return res;
+        }
     }
 }
