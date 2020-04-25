9f53a76ee3f20f5c54db4a126de1f1777ac8287c
Егор Люханов <ealuhanov@yandex.ru>
2020-04-26 01:55:23 +0500

    Stage 1 - Design v2 - Stable checkpoint

---
+++ Client/Pages/Log/MessageCollection.cs
@@ -0,0 +1,40 @@
+﻿namespace TKZ.Client.Pages.Log
+{
+    public class MessageCollection
+    {
+        //
+        //Branches messages
+        //
+        //Branches ID
+        public static Message Branch_IdError(bool IsStartId, string branchName)
+        {
+            string position = IsStartId ? "начала" : "конца";
+            return new Message(MessageClass.Branches, MessageType.Danger,
+                               $"Отсутствует узел {position} ветви!",
+                               $"Наименование ветви: {branchName}",
+                               "branches");
+        }
+
+        //
+        //Mutuals messages
+        //
+        //Mutuals ID
+        public static Message Mutual_IdError(bool IsStartId, string restBranchName)
+        {
+            string position = IsStartId ? "начала" : "конца";
+            return new Message(MessageClass.Mutuals, MessageType.Danger,
+                               $"Отсутствует ветвь {position} магнитосвязи!",
+                               $"Магнитосвязь ветви: {restBranchName}",
+                               "mutual_induction");
+        }
+
+        //Mutuals Orphan
+        public static Message Mutual_OrphanError()
+        {
+            return new Message(MessageClass.Mutuals, MessageType.Danger,
+                               $"Пустая магнитосвязь!",
+                               $"Магнитосвязь без ветвей.",
+                               "mutual_induction");
+        }
+    }
+}
---
+++ Client/Pages/Log/MessageEnums.cs
@@ -0,0 +1,21 @@
+﻿namespace TKZ.Client.Pages.Log
+{
+    //Message type
+    public enum MessageType
+    {
+        Success,
+        Warning,
+        Danger
+    }
+
+    //Message Class
+    public enum MessageClass
+    {
+        System,
+        IO,
+        Nodes,
+        Branches,
+        Mutuals,
+        Calculation
+    }
+}
