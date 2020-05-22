1de998af0bd581d1adaf6f18ea500db190834e5e
Егор Люханов <ealuhanov@yandex.ru>
2020-05-23 02:05:15 +0500

    Stage 1 - Design v2 - Advice. Manual integration mechanism. Part 1.

---
+++ Server/Controllers/AdviceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TKZ.Shared;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TKZ.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdviceController : ControllerBase
+    {
+        // GET
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return await System.IO.File.ReadAllTextAsync("calc.html");
+        }
+    }
+}
